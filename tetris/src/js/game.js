93365ce60db6d49378a4489dcae40de641436989
Andrey Nezhdanov <nezhd.an@yandex.ru>
2023-12-06 02:50:54 +0300

    feat: add view tetris

--- tetris/src/js/game.js
+++ tetris/src/js/game.js
@@ -2,41 +2,122 @@ export default class Game {
   score = 0;
   lines = 0;
   level = 0;
-  playfield = [
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-  ];
+  playfield = this.createPlayfield();
   // активная фигура
-  activePiece = {
-    x: 0,
-    y: 0,
-    get blocks() {
-      return this.rotations[this.rotationIndex]
-    },
-    blocks: [
-      [0,1,0],
-      [1,1,1],
-      [0,0,0],
-    ],
-  };
+  activePiece = this.createPiece();
+  // следующая фигура
+  nextPiece = this.createPiece();
+
+  // получить состояние
+  getState() {
+    const playfield = this.createPlayfield();
+    const { y: pieceY, x: pieceX, blocks } = this.activePiece;
+
+    for (let y = 0; y < this.playfield.length; y++) {
+      playfield[y] = [];
+      
+      for (let x = 0; x < this.playfield[y].length; x++) {
+        playfield[y][x] = this.playfield[y][x];
+      }
+    }
+
+    for (let y = 0; y < blocks.length; y++) {
+      for (let x = 0; x < blocks[y].length; x++) {
+        if (blocks[y][x]) {
+          playfield[pieceY + y][pieceX + x] = blocks[y][x];
+        }
+        
+      }
+      
+    }
+
+    return {
+      playfield
+    }
+  }
+
+  // создать игровое поле
+  createPlayfield() {
+    const playfield = [];
+
+    for (let y = 0; y < 20; y++) {
+      playfield[y] = [];
+      
+      for (let x = 0; x < 10; x++) {
+        playfield[y][x] = 0;
+      }
+    }
+
+    return playfield;
+  }
+
+  // создание фигуры
+  createPiece() {
+    const index = Math.floor(Math.random() * 7);
+    const type = 'IJLOSTZ'[index];
+    const piece = { x: 0, y: 0 };
+
+    switch (type) {
+      case 'I':
+        piece.blocks = [
+          [0,0,0,0],
+          [1,1,1,1],
+          [0,0,0,0],
+          [0,0,0,0],
+        ];
+        break;
+      case 'J':
+        piece.blocks = [
+          [0,0,0],
+          [2,2,2],
+          [0,0,2],
+        ];
+        break;
+      case 'L':
+        piece.blocks = [
+          [0,0,0],
+          [3,3,3],
+          [3,0,0],
+        ];
+        break;
+      case 'O':
+        piece.blocks = [
+          [0,0,0,0],
+          [0,4,4,0],
+          [0,4,4,0],
+          [0,0,0,0],
+        ];
+        break;
+      case 'S':
+        piece.blocks = [
+          [0,0,0],
+          [0,5,5],
+          [5,5,0],
+        ];
+        break;
+      case 'T':
+        piece.blocks = [
+          [0,0,0],
+          [6,6,6],
+          [0,6,0],
+        ];
+        break;
+      case 'Z':
+        piece.blocks = [
+          [0,0,0],
+          [7,7,0],
+          [0,7,7],
+        ];
+        break;
+      default:
+        throw new Error('Неизвестный тип фигуры')
+    }
+
+    piece.x = Math.floor((10 - piece.blocks[0].length) / 2);
+    piece.y = -1;
+
+    return piece;
+  }
 
   // движение влево
   movePieceLeft() {
@@ -59,10 +140,11 @@ export default class Game {
   // движение вниз
   movePieceDown() {
     this.activePiece.y += 1;
-    // если мы выходим за пределы поля, вернуть нас назад
+    // если мы выходим за пределы поля, вернуть нас назад и зафиксировать фигуру
     if (this.hasCollision()) {
       this.activePiece.y -= 1;
       this.lockPiece();
+      this.updatePieces();
     }
   };
 
@@ -114,20 +196,43 @@ export default class Game {
     return false;
   };
 
-  // перенос активной фигуры на игровое поле
+  // фиксируем активную фигуру на поле
   lockPiece() {
     // наша активная фигура и её координаты
     const { y: pieceY, x: pieceX, blocks } = this.activePiece;
+    // перекрашиваем её в единый цвет для неактивных фигур
+    const inactiveBlocks = this.updateColorInactivePiece(blocks);
+
     // перебираем строки
-    for (let y = 0; y < blocks.length; y++) {
+    for (let y = 0; y < inactiveBlocks.length; y++) {
       // перебираем элементы в каждой строке
-      for (let x = 0; x < blocks[y].length; x++) {
+      for (let x = 0; x < inactiveBlocks[y].length; x++) {
         // если блок есть
-        if (blocks[y][x]) {
+        if (inactiveBlocks[y][x]) {
           // копируем значения из массива blocks в массив playfield
-          this.playfield[pieceY + y][pieceX + x] = blocks[y][x];
+          this.playfield[pieceY + y][pieceX + x] = inactiveBlocks[y][x];
+
         }
       }
     }
+  }
+
+  // обновление активной и следующей фигуры
+  updatePieces() {
+    this.activePiece = this.nextPiece;
+    this.nextPiece = this.createPiece();
+  }
+
+  // обновление цвета у неактивной фигуры
+  updateColorInactivePiece(piece) {
+    for (let y = 0; y < piece.length; y++) {
+      for (let x = 0; x < piece[y].length; x++) {
+        if (piece[y][x] !== 0) {
+          piece[y][x] = 8;
+        }
+      }
+    }
+
+    return piece;
   }
 }
--- tetris/src/js/main.js
+++ tetris/src/js/main.js
@@ -1,9 +1,33 @@
 import Game from './game.js';
+import View from './view.js';
 
-// создаём экземпляр класса game
+const playfield = document.querySelector('.playfield');
+
 const game = new Game();
-// глобальному объекту window создаём свойство game и значением передаём нашу константу game
-// для того, чтобы получить доступ к объекту game
-window.game = game;
+const view = new View(playfield, 320, 640, 20, 10);
 
-console.log(game);
+window.game = game;
+window.view = view;
+
+document.addEventListener('keydown', (event) => {
+  switch (event.key) {
+    case 'ArrowLeft': // стрелка влево
+      game.movePieceLeft();
+      view.render(game.getState());
+      break;
+    case 'ArrowUp': // стрелка вверх
+      game.rotatePiece();
+      view.render(game.getState());
+      break;
+    case 'ArrowRight': // стрелка вправо
+      game.movePieceRight();
+      view.render(game.getState());
+      break;
+    case 'ArrowDown': // стрелка вниз
+      game.movePieceDown();
+      view.render(game.getState());
+      break;
+  }
+})
+
+view.render(game.getState());
