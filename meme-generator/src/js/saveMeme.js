b44bdd6d7d8c1a69a77ffab0e937be6ee5bae841
Andrey Nezhdanov <nezhd.an@yandex.ru>
2024-01-08 18:52:00 +0300

    fix: double save image

--- meme-generator/src/js/saveMeme.js
+++ meme-generator/src/js/saveMeme.js
@@ -1,5 +1,4 @@
 export default function saveMeme(canvas, button) {
   button.download = "image.png";
   button.href = canvas.toDataURL("image/png").replace(/^data:image\/[^;]/, 'data:application/octet-stream');
-  button.click();
 }
